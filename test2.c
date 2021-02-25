8b43b40f54eb9a50a392442970996f0b4876e3d4
hainl2000 <66208534+hainl2000@users.noreply.github.com>
2021-02-25 21:37:42 +0700

    Add files via upload

---
+++ test2.c
@@ -0,0 +1,203 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdio_ext.h>
+
+struct array
+{
+	int value;
+	struct array* next;
+};
+typedef struct _NODE
+{
+	char *tu;
+	int soLan;
+	struct array* e;
+	struct _NODE *left;
+	struct _NODE* right;
+}NODE;
+
+void push(struct array** root, int new_value)
+{
+	if(*root == NULL)
+	{
+		*root = (struct array*)calloc(1, sizeof(struct array));
+		(*root) -> next = NULL;
+		(*root) -> value = new_value;
+	}else{
+		if((*root) -> value != new_value)
+		{
+			struct array* new = (struct array*)calloc(1, sizeof(struct array));
+			new -> value = new_value;
+			new -> next = *root;
+			*root = new;
+		}
+	}
+}
+
+void InsertNode(NODE** root, char * _tu1, int _dongXH)
+{
+	if ((*root) == NULL)
+	{
+		(*root) = (NODE*)calloc(1, sizeof(NODE));
+		(*root) -> tu = (char*)malloc(sizeof(_tu1));
+		strcpy((*root)->tu,_tu1);
+		(*root)->soLan = 1;
+		(*root)->e = NULL;
+		push(&((*root) -> e), _dongXH);
+	}
+	else
+	{
+		if (strcmp((*root)->tu,_tu1 )< 0)
+		{	
+			InsertNode(&(*root)->right,_tu1,_dongXH);
+		}else if (strcmp((*root)->tu, _tu1) >0)
+		{
+			InsertNode(&(*root)->left, _tu1,_dongXH);
+		}
+		else if(strcmp((*root)->tu, _tu1) == 0)
+		{	
+			push(&((*root) -> e), _dongXH);	
+			(*root)->soLan +=1;
+		}
+	}
+}
+void Print2(struct array* a)
+{
+	while(a != NULL)
+	{
+		printf("%-5d", a -> value);
+		a = a -> next;
+	}
+	printf("\n");
+}
+
+void PrintBST(NODE* root)
+{
+	if (root != NULL)
+	{
+		PrintBST(root->left);
+		printf(" %-20s: %d \n",(root->tu),(root->soLan));
+		//printf("%s \n",root->tu);
+		PrintBST(root->right);
+	}
+}
+
+void PrintBST1(NODE* root)
+{	
+	if (root != NULL)
+	{	
+		//printf("12321\n");
+		PrintBST1(root->left);
+		printf("%-15s: %-3d : ",root->tu,root->soLan);
+		Print2(root->e);
+		printf("\n");
+		PrintBST1(root->right);
+	}
+}
+
+
+
+NODE* search(NODE* root,char* s)
+{
+	if(root == NULL)
+	{
+		return NULL;
+	}
+	else
+	{
+		if(strcmp(s,root->tu) == 0)
+			return root;
+		else if(strcmp(s , root->tu) < 0)
+			return search(root->left,s);
+		else if(strcmp(s,root->tu) > 0)
+			return search(root->right,s);
+	}
+}
+
+
+
+int CountNode(NODE* root) 
+{
+   if( root == NULL)
+      return 0;
+   else
+      return 1 + CountNode(root->left) + CountNode(root->right);
+}
+
+NODE* root = NULL;
+NODE* stopRoot = NULL;
+
+int main()
+{
+	FILE *f = fopen("alice30.txt","r");
+	FILE *f1 = fopen("stopw.txt","r");
+
+	char* s = calloc(100,sizeof(char));
+	char* s1 = calloc(100,sizeof(char));
+	char c;
+	int pos;
+	int row;
+	int stop;
+	int space;
+	int breakLine;
+	int i;
+
+	pos = 0;
+	while(!feof(f1))
+	{
+		fscanf(f1,"%s",s);
+		InsertNode(&stopRoot,s,pos);
+	}
+	//PrintBST(stopRoot);
+	//printf("xong 1\n");
+
+	int check = 0;
+	char c_prev;
+	i =0;
+	row = 1;
+	space = 0;
+	stop = 0;
+	breakLine = 0;
+	c = fgetc(f);
+	s1[0] = '\0';
+	while(c != EOF)
+	{	
+		if (c >= 'A' && c <= 'Z')
+		{	
+			if(c_prev == ' '||c_prev == '.'||c_prev == '-')
+			{	
+
+				check = 1;
+			}
+		}		
+		c = tolower(c);
+		if(c >= 'a' && c <= 'z' || c == 39)
+		{
+			stop = 1;
+			s1[i] = c;
+			++i;
+		}else{
+			if(stop == 1)
+			{
+				s1[i] = '\0';
+				if(search(stopRoot,s1) == NULL && check == 0)
+				{
+					InsertNode(&root, s1, row);
+				}
+				check = 0;
+				stop = 0;
+				i = 0;
+			}
+			if(c == '\n')
+				++row;
+		}
+		c_prev = c;
+		c = fgetc(f);
+	}
+	fclose(f);
+
+	PrintBST1(root);
+	printf("So tu la :%d\n",CountNode(root));
+}
